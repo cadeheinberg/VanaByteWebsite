de91e03174d44e260814e186c3ce8bc9b61ec7ad
BuildTools <unconfigured@null.spigotmc.org>
2024-05-15 19:17:38 +0000

    trying to fix afk mysql

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,63 @@ app.use(express.json());
 
 var host = '192.168.0.103';
 const port = 5000;
-const con = mysql.createConnection(config.db);
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+var con = null;
+
+//Todo:
+//1.may need to adjust wait/interactive timeout
+//2.may need to add a refresher setInterval to update the connection
+//3.may need to use "forever server.js"
+//https://stackoverflow.com/questions/12701259/how-to-make-a-node-js-application-run-permanently
+
+function handleConnection() {
+    console.log("Establishing a new connection to database")
+    con = mysql.createConnection(config.db);
+
+    //here we are creating a function that takes in error
+    const connectionError = function (err) {
+        if (err) {
+            console.log('Database Error 1', err);
+            //retry connection later
+            setTimeout(handleConnection, 2000);
+        } else {
+            console.log("Database connected!");
+        }
+    }
+
+    //now we pass this function into the connect as a parameter
+    //and connect will use this function inside of its own method
+    //inside of connection method it will 
+    //call "connectionError(null)" if sucessful, or
+    //call "connectionError(errInfo)" if not successful
+    con.connect(connectionError);
+
+    //you could also just put the function within the parameter
+    //slot itself instead of defining it before
+    //this is called an anonymous function i believe
+    //see below for an example
+
+    //set up a listener for any errors coming from MySQL
+    con.on('error', function (err) {
+        console.log('Database Error: 2', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleConnection();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleConnection();
 
 console.log("Test Query:")
 con.query("SELECT * FROM hub_stats LIMIT 1", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
 });
+
+//////
+////// ROUTING
+//////
 
 app.get("/stats", async (req, res) => {
     try {
@@ -29,9 +75,13 @@ app.get("/stats", async (req, res) => {
             res.json(result);
         });
     } catch (err) {
-        console.error(err.message);
+        if (err) console.error(err.message);
     }
 });
+
+//////
+////// LISTEN
+//////
 
 app.listen(port, host, () => {
     console.log(`Listening at ${host}:${port}`);
