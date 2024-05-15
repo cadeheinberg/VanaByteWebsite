ca41b02b1dbb35684907892eaa3e006f12968151
BuildTools <unconfigured@null.spigotmc.org>
2024-05-15 01:29:15 +0000

    node connects to mysql

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # VanaByteWebsite
 vanabyte.com
-test
+
+tar -xf node-v20.13.1-linux-x64.tar.xz
+sudo mv node-v20.13.1-linux-x64/bin/* /usr/local/bin/
+sudo mv node-v20.13.1-linux-x64/lib/node_modules/ /usr/local/lib/
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,33 @@
 var mysql = require('mysql');
 const express = require("express");
-const config = require('./credentials');
+const config = require('./db');
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
-app.get("/", (req, res) => {
-    res.json({ message: "ok" });
+
+const con = mysql.createConnection(config.db);
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
+
+app.get('/stats', (req, res) => {
+    con.query("SELECT * FROM hub_stats", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result)
+    });
+});
+
+app.get('/stats/:id', (req, res) => {
+    con.query("SELECT * FROM hub_stats WHERE UUID == ?", req.params, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result)
+    });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-const connection = mysql.createConnection(config.db);
