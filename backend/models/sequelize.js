c541c1c1d81eda3c06e571f9150b54233e978334
cadeheinberg <cadeheinberg@outlook.com>
2024-11-10 21:26:33 +0000

    update

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_VANABYTE_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false
